a4d72c115d7136feda0d2b70e99b68c8d5f605a4
iifast2 <60607576+medaminebt@users.noreply.github.com>
2025-06-23 21:51:41 +0100

    FIX - 1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay } from 'swiper/modules';
 
 
+
+
 export default function Footer() {
+
+  const [footerData, setFooterData] = useState<FooterData | null>(null);
+
+  useEffect(() => {
+    fetch('https://app.the-team.tn/api/landing-page')
+        .then(res => res.json())
+        .then(data => setFooterData(data));
+  }, []);
+
   return (
     <>
    {/* Footer Section */}
@@ -14,12 +26,30 @@ export default function Footer() {
     <div className="ji gp">
       <div className="tc uf ap gg fp">
         <div className="animate_top zd/2 to/4">
-          <a href="index.html">
+
+          {/* Map Column */}
+          <div className="animate_top flex justify-center items-center">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3193.0867425077995!2d10.152340376343352!3d36.84039856543168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd331d1cc461ed%3A0xea64759fdcb735cf!2sTHE%20TEAM%20%7C%20The%20Tunisian%20Establishment%20for%20Agile%20Management!5e0!3m2!1sfr!2stn!4v1750711497246!5m2!1sfr!2stn"
+                width="100%"
+                height="200"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Google Map"
+                className="rounded-lg shadow-md"
+            ></iframe>
+          </div>
+
+         {/* <a href="index.html">
             <img src="images/logoo.png" alt="Logo" className="om" />
             
           </a>
 
-          <p className="lc fb">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p className="lc fb">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>*/}
+
+
 
           <ul className="tc wf cg">
             <li>
@@ -34,28 +64,55 @@ export default function Footer() {
         </div>
 
 
+        <div className="grid grid-cols-3 gap-8">
 
+          <div className="animate_top">
+            <h4 className="kk wm tj ec">Contact</h4>
+            <ul>
+              <li>
+                <span className="sc xl vb">Address:</span>
+                <span className="ml-2">{footerData?.footer_address || 'Loading...'}</span>
+              </li>
+              <li>
+               {/* <span className="sc xl vb">Phone:</span>*/}
+                <span className="sc xl vb">{footerData?.footer_phone_number || 'Loading...'}</span>
+              </li>
+              <li>
+               {/* <span className="sc xl vb">Mobile:</span>*/}
+                <span className="sc xl vb">{footerData?.footer_mobile_number || 'Loading...'}</span>
+              </li>
+              <li>
+                <span className="sc xl vb">
+                <a
+                    href={`mailto:${footerData?.footer_contact_email || ''}`}
 
-       <div className="grid grid-cols-2 gap-8">
-  <div className="animate_top">
-    <h4 className="kk wm tj ec">Services</h4>
-    <ul>
-      <li><a href="#" className="sc xl vb">Training Offer</a></li>
-      <li><a href="#" className="sc xl vb">Consulting Services</a></li>
-      <li><a href="#" className="sc xl vb">Software Products</a></li>
-    </ul>
-  </div>
+                    className="ml-2 sc xl vb"
+                >
+                  {footerData?.footer_contact_email || 'Loading...'}
+                </a></span>
+              </li>
+            </ul>
+          </div>
 
-  <div className="animate_top">
-    <h4 className="kk wm tj ec">Support</h4>
-    <ul>
-      <li><a href="#" className="sc xl vb">The Team</a></li>
-      <li><a href="#" className="sc xl vb">Training</a></li>
-      <li><a href="#" className="sc xl vb">Our Blog</a></li>
-      <li><a href="#" className="sc xl vb">Contact Us</a></li>
-    </ul>
-  </div>
-</div>
+          <div className="animate_top">
+            <h4 className="kk wm tj ec">Services</h4>
+            <ul>
+              <li><a href="#" className="sc xl vb">Training Offer</a></li>
+              <li><a href="#" className="sc xl vb">Consulting Services</a></li>
+              <li><a href="#" className="sc xl vb">Software Products</a></li>
+            </ul>
+          </div>
+
+          <div className="animate_top">
+            <h4 className="kk wm tj ec">Support</h4>
+            <ul>
+              <li><a href="#" className="sc xl vb">The Team</a></li>
+              <li><a href="#" className="sc xl vb">Training</a></li>
+              <li><a href="#" className="sc xl vb">Our Blog</a></li>
+              <li><a href="#" className="sc xl vb">Contact Us</a></li>
+            </ul>
+          </div>
+        </div>
 
       </div>
     </div>
