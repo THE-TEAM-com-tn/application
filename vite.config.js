73f6bb13399df3a837408e9dc59ca68b2868a70c
iifast2 <60607576+medaminebt@users.noreply.github.com>
2025-08-27 15:28:51 +0100

    Fix - server redirection to index

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './', // Add this line
   plugins: [react()],
+  server: {
+    open: true,
+    // Configure server to return index.html for unknown routes
+    hmr: true,
+  },
 })
